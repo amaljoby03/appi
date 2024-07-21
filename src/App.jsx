58d359051a571f39bc2f7d9c669e65462d253502
Amal Joby <amalj589@gmail.com>
2024-07-21 21:13:05 +0530

    created state and added update function to update name instantaneously big gaga

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import NameInput from "./components/NameInput";
-import SaveButton from "./components/SaveButton";
+import { useState } from "react";
 
 function App() {
+  function UpdateName(event) {
+    setName(event.target.value);
+  }
+
+  function ShowPopup() {
+    alert("Hola!");
+  }
+
+  // const sum = addtwonumbers(4,5)
+  const [name, setName] = useState("John");
+
   return (
     <>
-      <NameInput />
-      <SaveButton />
+      <p> Current name:{name} </p>
+      <p>Please enter your Name:</p>
+      <input type="text" onChange={UpdateName}></input>
+      <button onClick={ShowPopup}>Save</button>
     </>
   );
 }
